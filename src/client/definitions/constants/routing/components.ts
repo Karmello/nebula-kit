706ba0fec86a61c3d239fc85e0bdd93e4c5926a9
Kamil Noga <nogakamil@gmail.com>
2025-08-31 16:49:46 +0000

    reogranized components folders

--- src/client/definitions/constants/routing/components.ts
+++ src/client/definitions/constants/routing/components.ts
@@ -17,8 +17,26 @@ export const COMPONENT_CATEGORIES = [
     ],
   },
   {
+    key: 'elements',
+    label: 'Elements',
+    items: [{ key: 'text', label: 'Text', sections: COMPONENT_SECTIONS }],
+  },
+  {
     key: 'controls',
     label: 'Controls',
     items: [{ key: 'button', label: 'Button', sections: COMPONENT_SECTIONS }],
+  },
+  {
+    key: 'layout-base',
+    label: 'Layout base',
+    items: [{ key: 'stack', label: 'Stack', sections: COMPONENT_SECTIONS }],
+  },
+  {
+    key: 'layouts',
+    label: 'Layouts',
+    items: [
+      { key: 'app-frame', label: 'AppFrame', sections: COMPONENT_SECTIONS },
+      { key: 'side-panel-layout', label: 'SidePanelLayout', sections: COMPONENT_SECTIONS },
+    ],
   },
 ]
