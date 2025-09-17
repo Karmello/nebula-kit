a2fa6035fe707c988dd9ab79a626bfac1f7c3086
Kamil Noga <nogakamil@gmail.com>
2025-09-17 09:39:32 +0000

    removed unused deps

--- scripts/distribute.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rimraf dist
-
-tsc --project tsconfig.dist.json
-tsc-alias --project tsconfig.dist.json
-
-sass --no-source-map ./src/lib/styles/index.scss ./dist/styles/index.css
-
-cp -R ./src/lib/assets ./dist
