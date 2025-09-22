13534fde4e6b565f2d458e717a5baa8cb8641160
Kamil Noga <nogakamil@gmail.com>
2025-09-22 18:28:22 +0000

    WithIcon docs

--- src/client/pages/components/ComponentPropsPage/PropsTable/props-table.tsx
+++ src/client/pages/components/ComponentPropsPage/PropsTable/props-table.tsx
@@ -1,4 +1,4 @@
-import { Section, Spacer, Table, Text } from 'lib/components'
+import { Icon, Section, Spacer, Table, Text } from 'lib/components'
 import { ComponentMeta } from 'client/definitions'
 
 type Props = {
@@ -14,29 +14,39 @@ export const PropsTable = ({ category, data }: Props) => {
           <Table.HeadCell>Name</Table.HeadCell>
           <Table.HeadCell tagAttrs={{ style: { minWidth: '15ch' } }}>Options</Table.HeadCell>
           <Table.HeadCell tagAttrs={{ style: { textAlign: 'center' } }}>Default</Table.HeadCell>
+          <Table.HeadCell tagAttrs={{ style: { width: '8ch' } }}>Required</Table.HeadCell>
+          <Table.HeadCell tagAttrs={{ style: { width: '10ch' } }}>Responsive</Table.HeadCell>
           <Table.HeadCell tagAttrs={{ style: { width: '40%', minWidth: '40ch' } }}>
             Description
           </Table.HeadCell>
         </Table.Row>
       </Table.Head>
       <Table.Body>
-        {Object.keys(data).map(name => {
-          const { options, isRequired, isResponsive, defaultValue, description } = data[name]
-          return (
-            <Table.Row key={name}>
-              <Table.Cell>
-                <Text intent="primary" bold>
-                  {`${name}${!isRequired ? '?' : ''}${isResponsive ? ' (resp)' : ''}`}
-                </Text>
-              </Table.Cell>
-              <Table.Cell>{typeof options === 'string' ? options : options.join(', ')}</Table.Cell>
-              <Table.Cell tagAttrs={{ style: { textAlign: 'center', whiteSpace: 'nowrap' } }}>
-                {defaultValue !== undefined ? defaultValue : '-'}
-              </Table.Cell>
-              <Table.Cell>{description}</Table.Cell>
-            </Table.Row>
-          )
-        })}
+        {Object.keys(data)
+          .sort((a, b) => a.localeCompare(b))
+          .map(name => {
+            const { options, isRequired, isResponsive, defaultValue, description } = data[name]
+            return (
+              <Table.Row key={name}>
+                <Table.Cell>
+                  <Text intent="primary" bold>
+                    {name}
+                  </Text>
+                </Table.Cell>
+                <Table.Cell>{typeof options === 'string' ? options : options.join(', ')}</Table.Cell>
+                <Table.Cell tagAttrs={{ style: { textAlign: 'center', whiteSpace: 'nowrap' } }}>
+                  {defaultValue !== undefined ? defaultValue : '-'}
+                </Table.Cell>
+                <Table.Cell tagAttrs={{ style: { textAlign: 'center' } }}>
+                  {isRequired ? <Icon name="check" intent="primary" /> : '-'}
+                </Table.Cell>
+                <Table.Cell tagAttrs={{ style: { textAlign: 'center' } }}>
+                  {isResponsive ? <Icon name="check" intent="primary" /> : '-'}
+                </Table.Cell>
+                <Table.Cell>{description}</Table.Cell>
+              </Table.Row>
+            )
+          })}
       </Table.Body>
     </Table>
   )
