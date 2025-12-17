cced16cc5c73747075dbe347386c9b6a6a6c4029
Kamil Noga <nogakamil@gmail.com>
2025-12-17 12:15:14 +0000

    fixing pw

--- playwright/index.tsx
+++ playwright/index.tsx
@@ -7,6 +7,9 @@ import 'lib/styles/index.scss'
 beforeMount(async ({ App, hooksConfig }) => {
   const { theme, brand } = hooksConfig ?? ({} as any)
 
+  document.documentElement.setAttribute('data-theme', theme)
+  document.documentElement.setAttribute('data-brand', brand)
+
   return (
     <NebkitProvider theme={theme} brand={brand}>
       <App />
