69049a9b208118a51a9ea975862e992760c99c82
Kamil Noga <nogakamil@gmail.com>
2026-01-18 18:36:17 +0000

    fixing examples page import bug

--- src/client/helpers/convertElemToString/convert-elem-to-string.tsx
+++ src/client/helpers/convertElemToString/convert-elem-to-string.tsx
@@ -1,5 +1,7 @@
 import { JSX } from 'react'
-import jsxToString from 'react-element-to-jsx-string'
+import reactElementToJSXString from 'react-element-to-jsx-string'
+
+const jsxToString = (reactElementToJSXString as any)?.default ?? reactElementToJSXString
 
 const FILTER_PROPS = ['key', 'ref']
 
@@ -7,6 +9,6 @@ export const convertElemToString = (elem: JSX.Element) => {
   return jsxToString(elem, {
     maxInlineAttributesLineLength: Infinity,
     sortProps: false,
-    filterProps: (value, key) => !FILTER_PROPS.includes(key),
+    filterProps: (value: any, key: any) => !FILTER_PROPS.includes(key),
   }).replaceAll(/(\r?\n[ \t]*<[^/][^>]*>)\s*\r?\n\s*([^<\r\n]+)\s*\r?\n\s*([ \t]*<\/[^>]+>)/g, '$1$2$3')
 }
