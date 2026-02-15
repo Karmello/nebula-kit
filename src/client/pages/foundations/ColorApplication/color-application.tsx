84f52f209a394ef91cc7ff5c6c6696052b7de71b
Kamil Noga <nogakamil@gmail.com>
2026-02-15 00:41:24 +0000

    disabled and loading refined

--- src/client/pages/foundations/ColorApplication/color-application.tsx
+++ src/client/pages/foundations/ColorApplication/color-application.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import { sentenceCase } from 'change-case'
 
-import { Box, Button, Grid, Select, Spacer, Text } from 'lib/components'
+import { Box, Button, Grid, Select, Spacer, Text, Switch, Flex } from 'lib/components'
 import { BOX_VARIANTS, BOX_INTENTS, BoxVariant } from 'lib/components/core/base/Box/definitions'
 import { COLORS } from 'lib/definitions'
 
 export default () => {
   const [variant, setVariant] = useState<BoxVariant>('solid')
+  const [disabled, setDisabled] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(false)
 
   return (
     <>
@@ -15,18 +17,30 @@ export default () => {
         in practice.
       </Text>
       <Spacer />
-      <Text bold>Variant</Text>
-      <Select
-        value={variant}
-        onChange={value => setVariant(value as BoxVariant)}
-        inlineSize="150px"
-        size="sm"
-        scrollAlign="center"
-      >
-        {BOX_VARIANTS.map(variant => (
-          <Select.Option value={variant}>{sentenceCase(variant)}</Select.Option>
-        ))}
-      </Select>
+      <Flex flexWrap="wrap" columnGap="15px" rowGap="10px">
+        <Flex.Item>
+          <Text bold>Variant</Text>
+          <Select
+            value={variant}
+            onChange={value => setVariant(value as BoxVariant)}
+            inlineSize="150px"
+            size="sm"
+            scrollAlign="center"
+          >
+            {BOX_VARIANTS.map(variant => (
+              <Select.Option value={variant}>{sentenceCase(variant)}</Select.Option>
+            ))}
+          </Select>
+        </Flex.Item>
+        <Flex.Item>
+          <Text bold>Disabled</Text>
+          <Switch checked={disabled} onChange={setDisabled} />
+        </Flex.Item>
+        <Flex.Item>
+          <Text bold>Loading</Text>
+          <Switch checked={loading} onChange={setLoading} />
+        </Flex.Item>
+      </Flex>
       <Spacer blockSize="50px" />
       <Box overflowX="auto">
         <Grid gridTemplateColumns="repeat(6, 1fr)" gap="7px">
@@ -34,7 +48,14 @@ export default () => {
             return BOX_INTENTS.map(intent => {
               return (
                 <Grid.Item key={`${color}_${intent}`}>
-                  <Button color={color} variant={variant} intent={intent} fullWidth>
+                  <Button
+                    color={color}
+                    variant={variant}
+                    intent={intent}
+                    fullWidth
+                    disabled={disabled}
+                    loading={loading}
+                  >
                     {intent} {color}
                   </Button>
                 </Grid.Item>
