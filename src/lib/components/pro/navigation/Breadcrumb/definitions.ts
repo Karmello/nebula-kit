64e73881534d0f6aa769ac553491e103e022c360
Kamil Noga <nogakamil@gmail.com>
2026-02-15 21:37:01 +0000

    removed !important's

--- src/lib/components/pro/navigation/Breadcrumb/definitions.ts
+++ src/lib/components/pro/navigation/Breadcrumb/definitions.ts
@@ -4,7 +4,7 @@ export const BREADCRUMB_TAGS = ['div', 'nav', 'section'] as const
 
 export const DEFAULT_BREADCRUMB_SIZE: BreadcrumbProps['size'] = 'xs'
 export const DEFAULT_BREADCRUMB_INTENT: BreadcrumbProps['intent'] = 'tertiary'
-export const DEFAULT_BREADCRUMB_ITEM_BORDER_INTENT: BreadcrumbProps['itemBorderIntent'] = 'primary'
+export const DEFAULT_BREADCRUMB_ITEM_BORDER_INTENT: BreadcrumbProps['itemBorderIntent'] = 'muted'
 
 export type BreadcrumbTag = (typeof BREADCRUMB_TAGS)[number]
 
