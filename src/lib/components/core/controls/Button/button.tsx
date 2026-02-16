9a2183acf9fa4dd717066e9338d237a52f40dbb8
Kamil Noga <nogakamil@gmail.com>
2026-02-16 14:47:19 +0000

    refining ripple

--- src/lib/components/core/controls/Button/button.tsx
+++ src/lib/components/core/controls/Button/button.tsx
@@ -112,7 +112,7 @@ export const Button = <T extends ButtonTag = 'button'>({
         text
       )}
       {loading && !disabled ? <Loader centered size={size} /> : null}
-      <Ripple parentRef={tagRef || ref} active={ripple} />
+      <Ripple parentRef={tagRef || ref} active={ripple && !loading && !disabled} />
     </Box>
   )
 }
