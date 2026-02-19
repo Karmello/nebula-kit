10c212725a7252b65d64cc693d98a15bc2684451
Kamil Noga <nogakamil@gmail.com>
2026-02-19 18:31:52 +0000

    retweaking colors

--- src/client/components/meta/CodeSnippet/code-snippet.tsx
+++ src/client/components/meta/CodeSnippet/code-snippet.tsx
@@ -12,39 +12,39 @@ export type CodeSnippetProps = {
 
 const COLOR_MAP = {
   // background
-  bg: 'var(--neb-gray-3)',
+  bg: 'var(--neb-gray-2)',
   // component name
-  '#F78C6C': 'var(--neb-red-8)',
+  '#F78C6C': 'var(--neb-red-5)',
   // native html tag name
-  '#CAECE6': 'var(--neb-red-8)',
+  '#CAECE6': 'var(--neb-red-5)',
   // prop name
-  '#C5E478': 'var(--neb-blue-8)',
+  '#C5E478': 'var(--neb-blue-5)',
   // object name, object key names
-  '#D6DEEB': 'var(--neb-gray-8)',
+  '#D6DEEB': 'var(--neb-gray-5)',
   // value
-  '#ECC48D': 'var(--neb-amber-9)',
+  '#ECC48D': 'var(--neb-amber-5)',
   // param name
-  '#D7DBE0': 'var(--neb-amber-9)',
+  '#D7DBE0': 'var(--neb-amber-5)',
   // argument name
-  '#FF5874': 'var(--neb-amber-9)',
+  '#FF5874': 'var(--neb-amber-5)',
   // called func name
-  '#82AAFF': 'var(--neb-blue-8)',
+  '#82AAFF': 'var(--neb-blue-5)',
   // TS type name
   '#FFCB8B': 'var(--neb-text)',
   // angle brackets, cb curly brackets
-  '#7FDBCA': 'var(--neb-gray-15)',
+  '#7FDBCA': 'var(--neb-gray-9)',
   // prop curly brackets
-  '#D3423E': 'var(--neb-gray-15)',
+  '#D3423E': 'var(--neb-gray-9)',
   // equal sign, arrow func sign, dot
-  '#C792EA': 'var(--neb-gray-15)',
+  '#C792EA': 'var(--neb-gray-9)',
   // quotes
-  '#D9F5DD': 'var(--neb-gray-15)',
+  '#D9F5DD': 'var(--neb-gray-9)',
   // comments
   '#637777': 'var(--neb-text)',
   // extension
-  '#5CA7E4': 'var(--neb-blue-8)',
+  '#5CA7E4': 'var(--neb-blue-5)',
   //
-  '#BAEBE2': 'var(--neb-blue-8)',
+  '#BAEBE2': 'var(--neb-blue-5)',
 }
 
 export const CodeSnippet = ({ code, lang = 'log' }: CodeSnippetProps) => {
