a9d8a81868b1ae879510d9c2bce95e7ee88b3e23
Kamil Noga <nogakamil@gmail.com>
2026-05-02 17:32:24 +0000

    AI questions updated

--- src/client/pages/app/AssistantPage/ChatAssistant/definitions.ts
+++ src/client/pages/app/AssistantPage/ChatAssistant/definitions.ts
@@ -10,13 +10,13 @@ Try asking:
 
 [How can you describe NebulaKit?](app://ask/How%20can%20you%20describe%20NebulaKit%3F)
 
-[How does drawing mechanism work?](app://ask/How%20does%20drawing%20mechanism%20work%3F)
+[What are composition and inheritance used for?](app://ask/What%20are%20composition%20and%20inheritance%20used%20for%3F)
+
+[How does NebulaKit render surfaces?](app://ask/How%20does%20NebulaKit%20render%20surfaces%3F)
+
+[What are orthogonal styling axes?](app://ask/What%20are%20orthogonal%20styling%20axes%3F)
 
 [How is responsiveness implemented?](app://ask/How%20is%20responsiveness%20implemented%3F)
-
-[How are composition and inheritance used?](app://ask/How%20are%20composition%20and%20inheritance%20used%3F)
-
-[What are orthogonal axes?](app://ask/What%20are%20orthogonal%20axes%3F)
 
 ---`
 
