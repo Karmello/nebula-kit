3237666f8bb68844dcddf43a02a56d48dcaa3f7c
Kamil Noga <nogakamil@gmail.com>
2026-05-06 16:33:10 +0000

    gap

--- src/client/components/app/App/AppFooter/app-footer.tsx
+++ src/client/components/app/App/AppFooter/app-footer.tsx
@@ -34,7 +34,7 @@ export const AppFooter = () => {
           Community & Support
         </Text>
         <Divider marginBottom="sm" />
-        <Flex flexDirection="column" flexWrap="nowrap" gap="xs">
+        <Flex flexDirection="column" flexWrap="nowrap" gap="2xs">
           <Link href="https://x.com/captainnebula" target="_blank">
             <Button color="gray" intent="inverse" size="sm" iconName="external-link" iconPlacement="right">
               X profile
