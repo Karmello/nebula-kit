79c84b01edc2eb468a9f0bce5a2f736a96defc7d
Kamil Noga <nogakamil@gmail.com>
2026-05-06 05:40:35 +0000

    edited SizingScale page

--- src/client/pages/foundations/SizingScale/sizing-scale.tsx
+++ src/client/pages/foundations/SizingScale/sizing-scale.tsx
@@ -6,18 +6,10 @@ export default () => {
   return (
     <Box maxInlineSize={{ lg: '55rem' }}>
       <Text>
-        Sizing scale defines the set of consistent values used for spacing and dimensions across the system. It replaces arbitrary
-        CSS values with a predictable scale, making layouts easier to reason about and maintain.
-      </Text>
-      <Spacer blockSize="xs" />
-      <Text>
-        The scale is used across length-related props such as margin, padding, blockSize or gap. It is the only sizing scale
-        available in NebulaKit.
-      </Text>
-      <Spacer blockSize="xs" />
-      <Text>
-        The scale follows a progressive step pattern, allowing small adjustments at lower values and larger jumps for layout-level
-        spacing.
+        The sizing scale defines the set of consistent values used for spacing and dimensions across the system. It is used in
+        layout-related props such as logical sizes as well as margin, padding and gap props. It replaces arbitrary CSS values with
+        a predictable scale, making layouts easier to reason about and maintain. The scale follows a progressive step pattern,
+        allowing small adjustments at lower values and larger jumps for layout-level spacing.
       </Text>
       <Spacer blockSize="xl" />
       <Table paddingBlock="10px" paddingInline="15px">
