9f4f2b9e64f8a33b7081197ad523c785ed155478
Kamil Noga <nogakamil@gmail.com>
2026-05-11 21:39:13 +0000

    updated Dockerfile

--- src/lib/components/core/overlays/DropdownList/dropdown-list.tsx
+++ src/lib/components/core/overlays/DropdownList/dropdown-list.tsx
@@ -3,7 +3,7 @@ import { useRef, useState } from 'react'
 import { FloatingResolved } from 'lib/components'
 import { WithSlots } from 'lib/components/core/internal'
 import { DEFAULT_BUTTON_SIZE } from 'lib/components/core/controls/Button/definitions'
-import { CONTROL_SIZE_TOKENS } from 'lib/definitions'
+import { CONTROL_SIZE_TOKENS, SIZING_SCALE } from 'lib/definitions'
 
 import {
   DropdownListProps,
@@ -77,7 +77,7 @@ export const DropdownList = ({
 
         const itemHeight =
           Number((CONTROL_SIZE_TOKENS[size || 'md'].blockSize as string).replace('px', '')) +
-          Number(getComputedStyle(document.documentElement).getPropertyValue('--neb-size-3xs').replace('px', ''))
+          Number(SIZING_SCALE['3xs'].replace('px', ''))
 
         return (
           <DropdownListProvider
