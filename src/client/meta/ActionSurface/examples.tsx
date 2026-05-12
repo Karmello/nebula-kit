3429dfdf8e78315bb17eafeb7e36d7bbcf943838
Kamil Noga <nogakamil@gmail.com>
2026-05-11 20:16:50 -0500

    updates examples

--- src/client/meta/ActionSurface/examples.tsx
+++ src/client/meta/ActionSurface/examples.tsx
@@ -3,7 +3,12 @@ import { ActionSurface, type ActionSurfaceProps } from 'lib/components'
 
 const ACTION_SURFACE_EXAMPLES_META: ComponentMeta<ActionSurfaceProps>['examples'] = [
   {
-    jsx: <ActionSurface>df</ActionSurface>,
+    jsx: (
+      <ActionSurface>
+        <ActionSurface.Heading>heading</ActionSurface.Heading>
+        <ActionSurface.Description>description</ActionSurface.Description>
+      </ActionSurface>
+    ),
   },
 ]
 
