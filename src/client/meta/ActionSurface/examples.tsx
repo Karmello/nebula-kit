bbd4769d707e41a46842a4b974d420c1f0ea5cc2
Kamil Noga <nogakamil@gmail.com>
2026-05-27 01:56:28 +0000

    save

--- src/client/meta/ActionSurface/examples.tsx
+++ src/client/meta/ActionSurface/examples.tsx
@@ -1,6 +1,6 @@
 import { ComponentMeta } from 'client/definitions'
 import { Text } from 'lib/components/core/Text'
-import { ActionSurface } from 'lib/components/core/ActionSurface/action-surface'
+import { ActionSurface } from 'lib/components/core/ActionSurface'
 import { type ActionSurfaceProps } from 'lib/components/core/ActionSurface/definitions'
 
 const ACTION_SURFACE_EXAMPLES_META: ComponentMeta<ActionSurfaceProps>['examples'] = [
