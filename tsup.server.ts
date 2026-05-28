83c44f402b96ec9752f3a3e875abd8342c202b6d
Kamil Noga <nogakamil@gmail.com>
2026-05-28 22:05:14 +0000

    tsup error

--- tsup.server.ts
+++ tsup.server.ts
@@ -3,12 +3,39 @@ import path from 'node:path'
 
 import { defineConfig } from 'tsup'
 
+const rawLoaderPlugin = {
+  name: 'raw-loader',
+
+  setup(build: any) {
+    build.onResolve({ filter: /\?raw$/ }, (args: any) => ({
+      path: path.resolve(args.resolveDir, args.path.replace(/\?raw$/, '')),
+      namespace: 'raw-loader',
+    }))
+
+    build.onLoad(
+      { filter: /.*/, namespace: 'raw-loader' },
+
+      async (args: any) => {
+        const contents = await fs.readFile(args.path, 'utf8')
+
+        return {
+          contents: `export default ${JSON.stringify(contents)}`,
+          loader: 'js',
+        }
+      }
+    )
+  },
+}
+
 export default defineConfig({
   entry: ['src/server/server.prod.ts'],
+
   platform: 'node',
   target: 'node20',
   format: ['esm'],
+
   outDir: 'build/server',
+
   sourcemap: false,
 
   external: ['react', 'react-dom', 'qs', 'object-inspect', 'side-channel'],
@@ -17,40 +44,12 @@ export default defineConfig({
     '.scss': 'text',
   },
 
+  esbuildPlugins: [rawLoaderPlugin],
+
   esbuildOptions(o) {
     o.logOverride = {
       'ignored-bare-import': 'silent',
     }
-
-    o.plugins = [
-      ...(o.plugins || []),
-
-      {
-        name: 'raw-loader',
-
-        setup(build) {
-          build.onResolve({ filter: /\?raw$/ }, args => {
-            return {
-              path: path.resolve(args.resolveDir, args.path.replace('?raw', '')),
-              namespace: 'raw-loader',
-            }
-          })
-
-          build.onLoad(
-            { filter: /.*/, namespace: 'raw-loader' },
-
-            async args => {
-              const contents = await fs.readFile(args.path, 'utf8')
-
-              return {
-                contents: `export default ${JSON.stringify(contents)}`,
-                loader: 'js',
-              }
-            }
-          )
-        },
-      },
-    ]
   },
 
   clean: false,
