ca6860ea40e48665eb719af01881b87b5963da9a
Kamil Noga <nogakamil@gmail.com>
2026-05-28 22:06:41 +0000

    tsup fixed locally

--- tsup.server.ts
+++ tsup.server.ts
@@ -1,59 +1,42 @@
 import fs from 'node:fs/promises'
 import path from 'node:path'
-
 import { defineConfig } from 'tsup'
 
 const rawLoaderPlugin = {
   name: 'raw-loader',
-
   setup(build: any) {
     build.onResolve({ filter: /\?raw$/ }, (args: any) => ({
       path: path.resolve(args.resolveDir, args.path.replace(/\?raw$/, '')),
       namespace: 'raw-loader',
     }))
-
-    build.onLoad(
-      { filter: /.*/, namespace: 'raw-loader' },
-
-      async (args: any) => {
-        const contents = await fs.readFile(args.path, 'utf8')
-
-        return {
-          contents: `export default ${JSON.stringify(contents)}`,
-          loader: 'js',
-        }
+    build.onLoad({ filter: /.*/, namespace: 'raw-loader' }, async (args: any) => {
+      const contents = await fs.readFile(args.path, 'utf8')
+      return {
+        contents: `export default ${JSON.stringify(contents)}`,
+        loader: 'js',
       }
-    )
+    })
   },
 }
 
 export default defineConfig({
   entry: ['src/server/server.prod.ts'],
-
   platform: 'node',
   target: 'node20',
   format: ['esm'],
-
   outDir: 'build/server',
-
   sourcemap: false,
-
   external: ['react', 'react-dom', 'qs', 'object-inspect', 'side-channel'],
-
   loader: {
     '.scss': 'text',
   },
-
   esbuildPlugins: [rawLoaderPlugin],
-
   esbuildOptions(o) {
     o.logOverride = {
       'ignored-bare-import': 'silent',
     }
   },
-
   clean: false,
-
   define: {
     'process.env.NODE_ENV': JSON.stringify('production'),
   },
