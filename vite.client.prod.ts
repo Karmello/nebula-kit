38bff4ece847f8900345f508c959ad6e98a1e967
Kamil Noga <nogakamil@gmail.com>
2026-05-29 05:11:16 +0000

    save

--- vite.client.prod.ts
+++ vite.client.prod.ts
@@ -34,7 +34,7 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            if (id.includes('/react-dom')) return 'react-dom'
+            // if (id.includes('/react-dom')) return 'react-dom'
             if (id.includes('/react-router')) return 'react-router'
             // if (id.includes('/react/')) return 'react'
             if (id.includes('/classnames')) return 'classnames'
