70b10feba21a30cbf8b4f6df7b09c0f9d184544a
Kamil Noga <nogakamil@gmail.com>
2026-06-03 05:09:55 +0000

    save

--- src/client/definitions/patterns/jsx/test.tsx
+++ src/client/definitions/patterns/jsx/test.tsx
@@ -6,6 +6,7 @@ export const Test = () => {
     cursor: 'pointer',
     flex: '1',
     interactive: true,
+    ripple: true,
     variant: 'solid',
     intent: 'tertiary',
     padding: 'xs',
@@ -13,13 +14,13 @@ export const Test = () => {
 
   return (
     <Flex drawable brand="blue" variant="solid" intent="tertiary" surface="dividing" inlineSize="300px" columnGap="3xs">
-      <Flex.Item {...flexItemProps} borderTopRightRadius="0px" borderBottomRightRadius="0px" ripple>
+      <Flex.Item {...flexItemProps} borderTopRightRadius="0px" borderBottomRightRadius="0px">
         <Text textAlign="center">Box 1</Text>
       </Flex.Item>
-      <Flex.Item {...flexItemProps} borderRadius="0px" ripple>
+      <Flex.Item {...flexItemProps} borderRadius="0px">
         <Text textAlign="center">Box 2</Text>
       </Flex.Item>
-      <Flex.Item {...flexItemProps} borderTopLeftRadius="0px" borderBottomLeftRadius="0px" ripple>
+      <Flex.Item {...flexItemProps} borderTopLeftRadius="0px" borderBottomLeftRadius="0px">
         <Text textAlign="center">Box 3</Text>
       </Flex.Item>
     </Flex>
