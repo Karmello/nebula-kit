6ac8ce32ed1060acb4733377a09d4f51b89525b5
Kamil Noga <nogakamil@gmail.com>
2026-06-04 18:16:08 +0000

    fixing flicker

--- src/client/components/app/App/app.tsx
+++ src/client/components/app/App/app.tsx
@@ -1,4 +1,4 @@
-import { useLayoutEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useLocation } from 'react-router'
 import { getHtmlMetaData } from 'src/server/helpers'
 
@@ -20,35 +20,42 @@ export const App = () => {
   const getUser = useGetUser(true, 300)
   const logoutUser = useLogoutUser()
 
-  const runAsync = async () => {
-    if (pathname.startsWith(PageKey.confirmAction)) {
-      await logoutUser.sendRequest()
-    } else {
-      const getUserRes = await getUser.sendRequest()
+  const [booting, setBooting] = useState(true)
 
-      if (!getUserRes.ok) {
-        await logoutUser.sendRequest()
-        if (pathname.startsWith('/profile')) {
-          navigateTo(PageKey.authLogin, { replace: true })
+  useEffect(() => {
+    const runAsync = async () => {
+      try {
+        if (pathname.startsWith(PageKey.confirmAction)) {
+          await logoutUser.sendRequest()
+        } else {
+          const getUserRes = await getUser.sendRequest()
+
+          if (!getUserRes.ok) {
+            await logoutUser.sendRequest()
+
+            if (pathname.startsWith('/profile')) {
+              navigateTo(PageKey.authLogin, { replace: true })
+            }
+          } else {
+            if (pathname.startsWith('/auth')) {
+              navigateTo(PageKey.profileAccount, { replace: true })
+            }
+          }
         }
-      } else {
-        if (pathname.startsWith('/auth')) {
-          navigateTo(PageKey.profileAccount, { replace: true })
-        }
+      } finally {
+        setBooting(false)
       }
     }
-  }
 
-  useLayoutEffect(() => {
     runAsync()
   }, [])
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     document.title = getHtmlMetaData(pathname).title
     window.scrollTo(0, 0)
   }, [pathname])
 
-  if (getUser.isMakingRequest || logoutUser.isMakingRequest) {
+  if (booting || getUser.isMakingRequest || logoutUser.isMakingRequest) {
     return (
       <Box blockSize="100dvh">
         <Loader centered size="lg" />
@@ -69,20 +76,25 @@ export const App = () => {
               <Toolbar.Start>
                 <PageNavigation toolbarSlot="start" mainOpen={mainOpen} setMainOpen={setMainOpen} />
               </Toolbar.Start>
+
               <Toolbar.Main>
                 <PageNavigation toolbarSlot="main" mainOpen={mainOpen} setMainOpen={setMainOpen} />
               </Toolbar.Main>
+
               <Toolbar.End>
                 <UserActionMenu />
               </Toolbar.End>
             </>
           )}
         </Toolbar>
+
         <AppJump />
       </AppFrame.Header>
+
       <AppFrame.Main paddingTop={{ base: 'md', lg: 'xl' }} paddingBottom="3xl">
         <RootPage />
       </AppFrame.Main>
+
       <AppFrame.Footer>
         <AppFooter />
       </AppFrame.Footer>
