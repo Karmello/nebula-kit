f3efe3b458ebb18c98ae23d4425e8a8a4e12c61b
Kamil Noga <nogakamil@gmail.com>
2026-06-08 11:05:02 +0000

    fix DEV

--- src/lib/components/core/AppFrame/slots/AppFrameHeader/definitions.ts
+++ src/lib/components/core/AppFrame/slots/AppFrameHeader/definitions.ts
@@ -1,5 +1,5 @@
-import { BoxColor, BoxIntent } from 'lib/components/core/Box'
-import { AppFrameProps, BoxProps } from 'lib/index.core'
+import { BoxColor, BoxIntent, BoxProps } from '../../../Box/types'
+import { AppFrameProps } from '../../definitions'
 
 export const DEFAULT_APP_FRAME_HEADER_INTENT: AppFrameHeaderProps['intent'] = 'muted'
 
